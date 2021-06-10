661c04f205fdf4362e992df3088d2674ce1b9a41
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-10 22:15:54 +0200

    Correzione Bonus

--- BONUS/ASSETS/JS/main.js
+++ BONUS/ASSETS/JS/main.js
@@ -1,38 +1,47 @@
 const app = new Vue({
+    /* Options */
     el: '#app',
-
     data: {
         url: 'https://flynn.boolean.careers/exercises/api/array/music',
-        musicData: [],
-        genres: [],
-        selectedGenre: 'All',
-
-    }, 
-
-    methods: {
-
+        disks: null,
+        error: null,
+        genres: ['all'],
+        selected: 'all',
+        sorted: null
     },
-
-    mounted() {
-        const fullUrl = this.url;
-        axios
-            .get(fullUrl)
-            .then(resp => {
-                this.musicData = resp.data.response;
-
-                this.musicData.forEach(element => {
-                    if(!this.genres.includes(element.genre)) {
-                        this.genres.push(element.genre)
-                    }
-                });
-                console.log(this.genres);
+    methods: {
+        fetchData(url) {
+            axios.get(url)
+                .then(resp => {
+                    this.disks = resp.data.response
+                })
+                .catch(e => {
+                    this.error = 'Could not connect to the API server! ' + e;
+                }).finally(() => {
+                    this.genres = [...this.genres, ...this.getGenres(this.disks)]
+                })
+        },
+        getGenres(data) {
+            const genres = []
+            data.forEach(el => {
+                if (!genres.includes(el.genre)) {
+                    genres.push(el.genre)
+                }
             })
-
-            .catch(e => {
-                console.error(e)
-        })
+            return genres;
+        },
+        sort(data) {
+            if (!this.sorted || this.sorted === 'ASC') {
+                this.disks = data.sort((a, b) => a.year - b.year)
+                this.sorted = 'DESC'
+            } else if (this.sorted === 'DESC') {
+                this.sorted = 'ASC'
+                this.disks = data.sort((a, b) => b.year - a.year)
+            }
+            console.log(this.disks);
+        }
+    },
+    mounted() {
+        this.fetchData(this.url);
     }
-
 })
-
-
